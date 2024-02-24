df28491964814477ce02fd99d88a62e3544303ed
Dylan Norquist <92738202+CutieillFusion@users.noreply.github.com>
2024-02-23 20:55:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ if __name__ == "__main__":
 # For anyone reading this code
 # - the resolution is FIXED, DONT TOUCH, nothing breaks but visually it stops working correctly
 # - buttons could be swapped to work with images but wasn't needed
-# - please don't fall into the trap abstracting graphic_component logic. Keep it seperate for sake of quick customization
-# - I have my a private key in here so don't upload to github with it
 # - the UI could look better but its the best programmer art I could obtain
 # - graphical components are stored in arrays. they are being modified based on index in update_temperature(), update_speed(), and set_weather_data()
 # - API calls tend to freeze the app for a second. I don't think adding multithreading is worth but its possible
@@ -30,9 +28,4 @@ if __name__ == "__main__":
 # - settings.py contains 90% of magic numbers and paths, modify anything easily from that file
 # - parsed_cities.json is required for this function correctly. if lost download current.city.list.json from OpenWeatherAPI and run parse_cities
 # - changing font ruins everything. fixing this issue seems very impossible
-# - all components are hardcoded in weather_management. Could be stored in a json file but that tends to make modifiying more frustrating from my experience
-    
-# pip install
-# - requests
-# - pygame
-# - pil
+# - all components positions and sizes are hardcoded in weather_management. Could be stored in a json file but that tends to make modifiying more frustrating from my experience
